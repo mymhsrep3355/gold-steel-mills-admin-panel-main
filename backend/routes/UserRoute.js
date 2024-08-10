b29535566d02c47f23a239d8c209ca79a9a74847
huzaifa003 <buntyhuzaifa3@gmail.com>
2024-08-11 00:32:40 +0500

    Fixed UserRoutes

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -4,14 +4,18 @@ const userController = require('../controller/UserController');
 const router = express.Router();
 const User = require('../models/User');
 
+router.get('/email/:email', userController.getUserByEmail); // Get user by email
+router.get('/id/:id', userController.getUserById); // Get user by ID
+router.get('/', userController.getAllUsers); // Get all users
+
 
 router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/?email=:email', userController.getUserByEmail);
-router.get('/?id=:id', userController.getUserById);
-router.get('/', userController.getAllUsers);
-router.put('/update', userController.updateUser);
-router.delete('/delete?id=:id', userController.deleteUser);
+
+router.delete('/delete/:id', userController.deleteUser); // Delete user by ID
+
+router.put('/update/:id', userController.updateUser); // Update user by ID
+router.put('/changeAccountStatus/:id', userController.changeAccountStatus); // Change account status by ID
 
 
 
