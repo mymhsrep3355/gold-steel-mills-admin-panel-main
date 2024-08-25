fffdfcf4c8893aab2053530869e6dc2b721b8430
hamza <muhammadhamzasalahuddin@gmail.com>
2024-08-25 17:43:21 +0500

    loader

--- frontend/src/pages/AuthLayout.jsx
+++ frontend/src/pages/AuthLayout.jsx
@@ -1,24 +1,46 @@
-import {Outlet, useNavigate} from 'react-router-dom'
-import {useAuthProvider} from "../hooks/useAuthProvider.js";
-import {useEffect} from "react";
-import {isTokenValid} from "../utils.js";
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useAuthProvider } from "../hooks/useAuthProvider.js";
+import { useEffect, useState } from "react";
+import { isTokenValid } from "../utils.js";
+import { Box } from '@chakra-ui/react';
+import Loader from './LandingPage/Loader/Loader.jsx';
 
-export const AuthLayout=()=>{
-    const navigate= useNavigate()
-    const {user,token}=  useAuthProvider()
+export const AuthLayout = () => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { user, token } = useAuthProvider();
 
-    // useEffect(() =>{
-    //     navigate("/")
-    // },[])
-    useEffect(()=>{
-        const isValid=isTokenValid(token)
-        if(user && isValid){
-            navigate("/")
-        }
-    },[user])
-    return <div className={'h-[100%]'}>
-        <div className={'h-[100%]'}>
-            <Outlet/>
-        </div>
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer); 
+  }, []);
+
+  
+  useEffect(() => {
+    if (!loading) {
+      const isValid = isTokenValid(token);
+      if (user && isValid) {
+        navigate("/");
+      }
+    }
+  }, [loading, user, token, navigate]);
+
+
+  if (loading) {
+    return (
+      <Box m={"auto"} mt={"600px"}>
+        <Loader />
+      </Box>
+    );
+  }
+
+  return (
+    <div className={'h-[100%]'}>
+      <div className={'h-[100%]'}>
+        <Outlet />
+      </div>
     </div>
-}
+  );
+};
+
+export default AuthLayout;
